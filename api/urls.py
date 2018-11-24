3ba27e0ad7d201f6bc0ba2ae617c8d9bea215328
Przemek Pawlas <przemyslaw.pawlas@xsolve.pl>
2018-11-24 22:35:32 +0100

    Create game endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
+    url(r'api/games/initialize/$', views.initialize_game, name='initialize_game'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import Distance
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+import datetime
+import random
 
-# Create your views here.
+@api_view(['POST'])
+def initialize_game(request):
+    data = request.data
+    number_of_locations = data.get('number_of_locations', '')
+
+    if number_of_locations < 4 or number_of_locations > 6:
+        return Response({'error': 'Invalid number of locations, should be 4-6'}, status=status.HTTP_400_BAD_REQUEST)
+
+    point = data.get('point', '')
+
+    locations_within_radius = Location.objects.filter(
+        point__distance_lt=(Point(point[1], point[0]), Distance(km=data.get('radius', ''))))
+    if len(locations_within_radius) < number_of_locations:
+        return Response({'error': 'Not enough locations in the area around the chosen point'}, status=status.HTTP_400_BAD_REQUEST)
+
+    locations = random.sample(locations_within_radius, number_of_locations)
+
+    questions = []
+
+    for location in locations:
+        question = LocationQuestion.objects.filter(location=location).order_by('?').first()
+        if question is None:
+            return Response({'error': 'No question for ' + location.name}, status=status.HTTP_400_BAD_REQUEST)
+
+        questions.append(question)
+
+    user = User.objects.get(pk=data.get('user_id', ''))
+
+    game = Game.objects.create(
+        create_date=datetime.datetime.now(),
+        owner=user,
+        users=[user],
+        locations=locations,
+        questions=questions
+    )
+
+    serializer = GameSerializer(game)
+
+    if serializer.is_valid():
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
