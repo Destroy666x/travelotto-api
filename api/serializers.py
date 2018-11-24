04626957f82f13d88596a1ae5db19a0e4a1cd54d
Bartosz Maciaszek <bartosz.maciaszek@gmail.com>
2018-11-24 17:26:00 +0100

    serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,20 +2,24 @@ from rest_framework import serializers
 from .models import *
 
 
-class GameSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Game
-        exclude = ()
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        exclude = ()
-        # exclude = ('password',)
-
-
-class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
+        exclude = ()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile: UserProfileSerializer()
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
+class GameSerializer(serializers.ModelSerializer):
+    users = UserSerializer(many=True)
+    owner = UserSerializer()
+
+    class Meta:
+        model = Game
         exclude = ()
