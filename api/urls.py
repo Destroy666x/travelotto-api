45ece2037169f549248a1563dcb5aff2ed3b6944
Przemek Pawlas <przemyslaw.pawlas@xsolve.pl>
2018-11-25 04:42:06 +0100

    Lottery endpoint

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'api/games/initialize/$', views.initialize_game, name='initialize_game'),
     path('api/games/<int:game_id>/visit/<int:location_id>/', views.visit_location, name='visit_location'),
     path('api/games/<int:game_id>/answer/<int:location_id>/', views.answer_question, name='answer_question'),
+    path('api/games/<int:game_id>/lottery/', views.get_game_lottery, name='get_game_lottery'),
 ]
